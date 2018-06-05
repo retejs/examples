041c9b53f991fe0896461102219834ee493fa5e7
Vitaliy Stoliarov <huccah@gmail.com>
2018-06-05 15:27:23 +0300

    update nodejs example

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,23 +1,27 @@
-var { Engine, ComponentWorker } = require('d3-node-editor/build/d3-node-editor.engine');
+var { Engine, Component } = require('rete/build/rete.engine');
 
-var c1 = new ComponentWorker('component1', {
+class Comp extends Component {
+    constructor() {
+        super('component1');
+    }
+
     worker(node, inputs, outputs) {
         console.log('worker')
         outputs[0] = 1;
     }
-});
+}
 
-var engine = new Engine('name@0.0.0', [c1]);
+var engine = new Engine('name@0.0.0');
+engine.register(new Comp());
 
 engine.process({
     id: 'name@0.0.0',
     nodes: {
         1: {
-            title: 'component1',
+            name: 'component1',
             id: 1,
             inputs: [],
             outputs: []
         }
-    },
-    groups: { }
+    }
 }).then(console.log)
