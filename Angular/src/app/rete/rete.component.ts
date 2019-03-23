ad6b1f023d82ed2a1940550aedb5e959fa4d5e67
Vitaliy Stoliarov <huccah@gmail.com>
2019-03-23 20:50:34 +0200

    update rete for Angular exaple

--- Angular/src/app/rete/rete.component.ts
+++ Angular/src/app/rete/rete.component.ts
@@ -58,7 +58,7 @@ export class ReteComponent implements AfterViewInit {
     editor.connect(n2.outputs.get('num'), add.inputs.get('num2'));
 
 
-    editor.on('process nodecreated noderemoved connectioncreated connectionremoved', (async () => {
+    editor.on('process nodecreated noderemoved connectioncreated connectionremoved' as any, (async () => {
       await engine.abort();
       await engine.process(editor.toJSON());
     }) as any);
